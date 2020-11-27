ed6475ca989e355d15dc1bd72172c425891b2a16
njohou landry <landrynjohou999@gmail.com>
2020-11-27 12:50:31 +0100

    mise à jour du diagramme de classe

--- icc_intership_project/backend/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/njohou/Bureau/Project_INTERSHIP/INTERSHIP_PROJECT/icc_intership_project/backend/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
