41c5b646e6e673373f597b4acceff4656e88d9d7
Christophenkoa <nkoachristophe@gmail.com>
2020-12-31 15:08:25 +0100

    authentication ended

--- front-end-project/src/app/services/auth-guard/auth.service.ts
+++ front-end-project/src/app/services/auth-guard/auth.service.ts
@@ -1,14 +1,8 @@
-import {EventEmitter, Injectable} from '@angular/core';
-import {HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders} from '@angular/common/http';
-import { CanActivate, Router } from '@angular/router';
+import {EventEmitter, Injectable, OnInit} from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
-import {Observable, Subject} from 'rxjs';
-import { tap, shareReplay } from 'rxjs/operators';
-
-// import * as jwtDecode from 'jwt-decode';
-import jwt_decode from 'jwt-decode';
-import * as moment from 'moment';
-// import Swal from 'sweetalert2';
+import {Observable, Subscription, throwError} from 'rxjs';
 
 
 import {JwtHelperService} from '@auth0/angular-jwt';
@@ -19,11 +13,14 @@ import {JwtHelperService} from '@auth0/angular-jwt';
 })
 export class AuthService {
   authentication = new EventEmitter<boolean>();
+  tokenSubscription = new Subscription();
+  user: User = null;
+  httpHeaders = null;
+  timeout;
 
-  // @ts-ignore
-  httpHeaders = new HttpHeaders({'Content-type': 'application/json', 'Authorization': 'JWT ' + this.getToken() });
-
-  constructor(public jwtHelper: JwtHelperService, private router: Router) { }
+  constructor(public jwtHelper: JwtHelperService,
+              private userManager: UserManagerService,
+              private router: Router) { }
 
   public isAuthenticated(): boolean {
     const token = localStorage.getItem('token');
@@ -31,7 +28,47 @@ export class AuthService {
     return !this.jwtHelper.isTokenExpired(token);
   }
 
+  login(username: string, password: string) {
+    this.userManager.authenticate(username, password).subscribe(
+      result => {
+
+        const loginResult = result as Token;
+
+        this.httpHeaders = new HttpHeaders({'Content-type': 'application/json', 'Authorization': 'JWT ' + loginResult.token });
+
+        console.log(result);
+        this.authentication.emit(true);
+        localStorage.setItem('auth', '' + true);
+
+        // Here we are storing the token and refresh token in the localstorage
+        localStorage.setItem('token', loginResult.token);
+
+        const decoded = jwt_decode<JwtPayload>(loginResult.token);
+        console.log(decoded);
+
+        localStorage.setItem('expire_time', '' + decoded.exp);
+        this.timeout = decoded.exp;
+
+        this.user = loginResult.user;
+        localStorage.setItem('id', '' + this.user.id);
+        localStorage.setItem('username', '' + this.user.username);
+        localStorage.setItem('is_superuser', '' + this.user.is_superuser);
+        localStorage.setItem('is_staff', '' + this.user.is_staff);
+        localStorage.setItem('is_active', '' + this.user.is_active);
+
+        this.router.navigate(['/quiz']);
+      },
+
+      error => {
+        console.log(error);
+      }
+    );
+  }
   logout() {
+    this.tokenSubscription.unsubscribe();
+    this.authentication.emit(false);
+    this.router.navigate(['/login']);
+
     localStorage.removeItem('token');
     localStorage.removeItem('id');
     localStorage.removeItem('username');
@@ -40,15 +77,58 @@ export class AuthService {
     localStorage.removeItem('is_superuser');
     localStorage.removeItem('is_staff');
     localStorage.removeItem('is_active');
-    this.authentication.emit(false);
-    this.router.navigate(['/login']);
   }
 
   getToken(): string {
     return localStorage.getItem('token');
   }
+  }
+interface JwtPayload {
+  exp: number;
+  user_id: number;
+  username: string;
+}
 
-  getAccesLevel() {
-    // implement
+interface Token {
+  token: string;
+  user: User;
+}
+
+interface User {
+  id: number;
+  username: string;
+  is_superuser: boolean;
+  is_staff: boolean;
+  is_active: boolean;
+}
+
+import {
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import {catchError, delay, finalize} from 'rxjs/operators';
+import jwt_decode from 'jwt-decode';
+import {UserManagerService} from '../user/user-manager.service';
+
+// import 'rxjs/add/operator/do';
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    delay(10000);
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error && error.status === 401) {
+          // 401 handled in auth.interceptor
+          this.authService.logout();
+        }
+        return throwError(error);
+      })
+    );
   }
 }
